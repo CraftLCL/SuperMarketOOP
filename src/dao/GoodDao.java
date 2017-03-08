9526375d8da4982a72408aa85a7485795ade3d3a
CraftLCL <lcl246879513@outlook.com>
2017-03-08 20:28:17 +0800

    update

--- src/dao/GoodDao.java
+++ src/dao/GoodDao.java
@@ -4,13 +4,11 @@ import entity.Good;
 
 import java.util.List;
 
-/**
- * Created by lcl on 2017/3/5.
- */
 public interface GoodDao {
      void addGood(Good good);
      void deleteGood(String id);
      void updateGood(Good good);
      Good findById(String id);
+     List<Good> findByName(String name);
      List<Good> findAll();
 }
